538b224c0731c5046c3070688a1605e0e3abd17a
MattiaPlacido <placido.mattia@gmail.com>
2024-10-14 18:53:07 +0200

    Concluso

--- main.js
+++ main.js
@@ -7,12 +7,44 @@
 // l'output del prezzo va stampato con massimo 2 decimali
 
 // RACCOLTA DATI
-//chiedo all'utente i dati e li converto in numeri
-const age = parseInt(prompt("Inserisci la tua età"));
-const distance = parseInt(prompt("Quanti km devi viaggiare?"));
+
+//dichiarazione variabili di questa sezione
+const age = parseInt(prompt("Inserisci la tua età")); //chiedo all'utente i dati e li converto in numeri
+const distance = parseInt(prompt("Quanti km devi viaggiare?")); //chiedo all'utente i dati e li converto in numeri
 
 console.log(age, distance); //da togliere
 // ELABORAZIONE
+
+//dichiarazione variabili di questa sezione
+let price = distance * 0.21;
+let discount = 1;
 const isAgeValid = age > 0 && age < 120 && !isNaN(age); //l'età valida va da 1 a 119 anni
+const isDistanceValid = distance > 0 && !isNaN(distance);
+
+//controllo che i dati inseriti siano validi
+if (!isAgeValid || !isDistanceValid) {
+  if (!isAgeValid && !isDistanceValid) {
+    console.log("Entrambi i dati inseriti non sono validi!");
+  } else if (!isAgeValid) {
+    console.log("L'età inserita non è valida!");
+  } else {
+    //se entrambe le condizioni precedenti non sono soddisfatte e si è dentro l'if (!isAgeValid || !isDistanceValid) questo è l'unico output rimanente, quindi non c'è bisogno di condizione
+    console.log("La distanza inserita non è valida!");
+  }
+}
+
+//imposto gli sconti per età
+if (age < 18) {
+  discount = 0.8;
+} else if (age >= 65) {
+  discount = 0.6;
+}
+
+//calcolo il prezzo finale
+price = price * discount;
 
 // OUTPUT
+if (isAgeValid && isDistanceValid) {
+  // comunico l'output solo se i dati sono validi
+  console.log(`Il biglietto costa ${price}€!`);
+}
